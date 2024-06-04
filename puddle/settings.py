e4867a95e11938c7f3d67c355a6b5b7998bc8f23
gagoalaverdyan <gagoalaverdyan@gmail.com>
2024-06-05 01:17:27 +0400

    Implemented dashboard

--- puddle/settings.py
+++ puddle/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core.apps.CoreConfig",
+    "dashboard.apps.DashboardConfig",
     "item.apps.ItemConfig",
 ]
 
